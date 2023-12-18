4f4719ce26c40adebbb5aff61d60f7dd359b424d
Upul <upulkr4@gmail.com>
2023-12-18 22:45:39 +0530

    index.js

--- index.js
+++ index.js
@@ -27,9 +27,6 @@ app.post("/api", async (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("hellow docker");
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
